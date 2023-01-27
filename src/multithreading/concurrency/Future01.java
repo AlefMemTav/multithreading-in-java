87f7be2923fbd6d2585859bbc5842379f0f9f441
AlefMemTav <190115548@aluno.unb.br>
2023-01-27 12:08:24 -0300

    Executors part 6 Future

---
+++ src/multithreading/concurrency/Future01.java
@@ -0,0 +1,27 @@
+package multithreading.concurrency;
+
+import java.util.concurrent.*;
+
+public class Future01 {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<Double> dollarRequest = executorService.submit(() -> {
+            TimeUnit.SECONDS.sleep(3);
+            return 4.35D;
+        });
+        System.out.println(doSomething());
+        Double dollarResponse = dollarRequest.get(15, TimeUnit.SECONDS);
+        System.out.println("Dollar: " + dollarResponse);
+        executorService.shutdown();
+    }
+
+    private static long doSomething() {
+        System.out.println(Thread.currentThread().getName());
+        long sum = 0;
+        for (int i = 0; i < 1_000_000; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}

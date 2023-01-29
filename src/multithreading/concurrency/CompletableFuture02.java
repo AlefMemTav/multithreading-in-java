6863fe7fff5cf4f5c08522677cc0f4b598b6984a
AlefMemTav <190115548@aluno.unb.br>
2023-01-29 14:17:13 -0300

    CompletableFuture part 02

---
+++ src/multithreading/concurrency/CompletableFuture02.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package multithreading.concurrency;
+
+/**
+ *
+ * @author Micro
+ */
+public class CompletableFuture02 {
+    
+}

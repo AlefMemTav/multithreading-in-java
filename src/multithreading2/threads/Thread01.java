f7d4e129ec1eeb564cd6d6be39884235c9d2b407
AlefMemTav <190115548@aluno.unb.br>
2023-04-24 00:11:22 -0300

    Update Thread 01

--- src/multithreading2/threads/Thread01.java
+++ src/multithreading2/threads/Thread01.java
@@ -12,13 +12,13 @@ public class Thread01 {
         Thread t0 = new Thread(runnable);
         //t1.run(); // It will execute the thread t1 on the same thread t, without creating a new thread
         t0.start(); // To create a new thread you must use the method start (and not use run method)
-        
+
         // To create a new thread with runnable using lambda function
         Thread t1 = new Thread(() -> System.out.println(Thread.currentThread().getName())); // output: Thread-1
         t1.start();
         //t1.start(); // DO NOT DO IT! Never try execute the same thread twice
-        
-        Thread t2 = new Thread(new Thread01Runnable()); // You can not execute the same thread twice, but you can execute the same runnable twice, incluse the same instance
+
+        Thread t2 = new Thread(new Thread01Runnable());
         t2.start();
     }
 }
